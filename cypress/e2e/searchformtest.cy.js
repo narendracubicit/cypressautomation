c05aca9f3c6c45266ab2b8f6ad91d754414dce79
narendracubicit <pallanarendra3@gmail.com>
2023-05-03 16:22:28 +0530

    Assignment file doesnot have website url earlier

--- cypress/e2e/searchformtest.cy.js
+++ cypress/e2e/searchformtest.cy.js
@@ -14,28 +14,29 @@ describe('QA Search - Validate search form', () => {
   })
 
   it('Scenario 02: Verify submit button funcationality with empty search value', () => {
-    mainpage.enterTextInSearchInputTextBox(''); // empty value
+    // did not enter search value and clicking on submit button
     mainpage.clickSearchSubmitButton();
     // empty query error message should be displayed.
     mainpage.getEmptyQueryErrorMessage().should('be.visible');
-    // should not be displayed with any result set
-    mainpage.getSearchResultsList().should('not.be.visible');
+    // search results should not be existed.
+    mainpage.getSearchResultsList().should('not.exist');
   })
 
   it('Scenario 03: Verify search results when user searches for isla - atleast one island should be returned', () => {
     mainpage.enterTextInSearchInputTextBox('isla');
     mainpage.clickSearchSubmitButton();
     // search result should be visbile and at least 1 result should be displayed
-    mainpage.getSearchResultsList().should('be.visible').and('be.at.least', 1);
+    mainpage.getSearchResultsList().should('be.visible')
+    mainpage.getSearchResultsList().should('have.length.least', 1)
   })
 
   it('Scenario 04: Verify feedback message when no search results are found', () => {
     mainpage.enterTextInSearchInputTextBox('castle');
     mainpage.clickSearchSubmitButton();
     // no results found message should be displayed.
-    mainpage.getNoResultsErrorMessage.should('be.visible');
+    mainpage.getNoResultsErrorMessage().should('be.visible');
     // should not display any search results
-    mainpage.getSearchResultsList().should('not.exist');
+    mainpage.getSearchResultsList().should('not.exist')
   })
 
   it('Scenario 05: Verify the search results for query port', () => {
